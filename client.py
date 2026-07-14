4df220edb407e6e51c0c6465c84b2c2bb3d6089d
Leonardo-Corsini <65716378+Leonardo-Corsini@users.noreply.github.com>
2026-07-14 21:40:06 +0200

    Thread logic started

--- client.py
+++ client.py
@@ -1,21 +1,22 @@
 import socket
+import time
+
 from shared import pack_message, MSG_TYPE_HANDSHAKE
 
 def main():
-    """
-    Main function for the TimeStamping Client.
-    """
-    host = '127.0.0.1'  # The server's hostname or IP address
-    port = 65432        # The port used by the server
+    host = '127.0.0.1'
+    port = 65432
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         print(f"Connected to server at {host}:{port}")
-        
-        message = "Hello, World!"
-        packed_message = pack_message(MSG_TYPE_HANDSHAKE, message.encode())
-        s.sendall(packed_message)
-        print(f"Sent to server: {message}")
+
+        while True:
+            message = "Hello, from Bob!"
+            packed_message = pack_message(MSG_TYPE_HANDSHAKE, message.encode())
+            s.sendall(packed_message)
+            print(f"Sent to server: {message}")
+            time.sleep(1)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,29 +1,38 @@
 import socket
 from shared import unpack_message
+import threading
 
+class ClientHandler(threading.Thread):
+    def __init__(self, conn, addr):
+        super().__init__()
+        self.conn = conn
+        self.addr = addr
 
-def main():
-    """
-    Main function to run the TimeStamping Server.
-    """
-    host = '127.0.0.1'  # Standard loopback interface address (localhost)
-    port = 65432        # Port to listen on (non-privileged ports are > 1023)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        print(f"Server listening on {host}:{port}")
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
+    def run(self):
+        with self.conn:
             while True:
-                msg_type, payload = unpack_message(conn)
+                msg_type, payload = unpack_message(self.conn)
                 if msg_type is None:
                     break
                 print(f"Received message type: {msg_type}")
                 print(f"Received payload: {payload.decode()}")
-                # Message handling logic will go here
-                # ciao
+
+
+
+def main():
+    host = '127.0.0.1'
+    port = 65432
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        while True:
+            s.listen()
+            print(f"Server listening on {host}:{port}")
+            conn, addr = s.accept()
+            print(f"Connected by {addr}")
+            client_handler = ClientHandler(conn, addr)
+            client_handler.start()
 
 if __name__ == '__main__':
     main()
